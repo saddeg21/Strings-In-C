988a619c4fcf2483d0b2fee4baba132bc04db5d1
Ahmet Deniz Daştan <89015461+saddeg21@users.noreply.github.com>
2022-09-29 12:35:29 +0300

    v1 committed

---
+++ GATE-2015 RECURSION WITH POINTER/recursion-with-pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void foo(char *a) {
+    if(*a && *a != ' ') {
+        foo(a+1);
+        putchar(*a);
+    }
+}
+
+int main() {
+    foo("ABCD EFGH");
+    return 0;
+}
